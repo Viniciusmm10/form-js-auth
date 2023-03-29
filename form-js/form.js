c9ae406895c0a395154235150b0461f5ad8b7d16
viniciusmm10 <viniciusmm10@outlook.com>
2023-03-29 17:20:32 -0300

    mod29.03

--- form-js/form.js
+++ form-js/form.js
@@ -1,13 +1,15 @@
-// AQUI DEFINE QUE OS BOTÔES DE LOGIN E "ESQUECI A SENHA" FIQUEM ATIVOS APENAS SE TIVER UM EMAIL E SENHA VALIDOS NOS CAMPOS DE INPUT
-function validateFields() {
-  const emailValid = isEmailValid()
-  document.getElementById('recover-password-button').disabled = !emailValid
-
-  const passwordValid = isPasswordValid()
-  document.getElementById('login-button').disabled =
-    !emailValid || !passwordValid
+// define se os botões de login e "esqueci a senha" devem ficar ativos apenas se houver um email e senha válidos nos campos de input
+function onChangeEmail() {
+  toggleButtonDisable()
+  toggleEmailError()
 }
 
+function onChangePassword() {
+  toggleButtonDisable()
+  togglePasswordError()
+}
+
+// verifica se o email inserido é válido
 function isEmailValid() {
   const email = document.getElementById('email').value
   if (!email) {
@@ -16,6 +18,33 @@ function isEmailValid() {
   return validateEmail(email)
 }
 
+// mostra/oculta o erro do email e verifica se o email inserido é válido
+function toggleEmailError() {
+  const email = document.getElementById('email').value
+  const emailInvalidError = document.getElementById('email-invalid-error')
+
+  if (validateEmail(email)) {
+    emailInvalidError.style.display = 'none'
+  } else {
+    emailInvalidError.style.display = 'block'
+  }
+}
+
+// mostra/oculta o erro da senha
+function togglePasswordError() {
+  const password = document.getElementById('password').value
+  const passwordRequiredError = document.getElementById(
+    'password-required-error'
+  )
+
+  if (!password) {
+    passwordRequiredError.style.display = 'block'
+  } else {
+    passwordRequiredError.style.display = 'none'
+  }
+}
+
+// verifica se a senha inserida é válida
 function isPasswordValid() {
   const password = document.getElementById('password').value
   if (!password) {
@@ -24,12 +53,22 @@ function isPasswordValid() {
   return true
 }
 
-function validateEmail(email) {
-  return /\S+@\S+\.\S+/.test(email)
+// verifica se o email e a senha inseridos são válidos e habilita/desabilita os botões de login e "esqueci a senha"
+function toggleButtonDisable() {
+  const emailValid = isEmailValid()
+  const passwordValid = isPasswordValid()
+
+  document.getElementById('recover-password-button').disabled = !emailValid
+  document.getElementById('login-button').disabled =
+    !emailValid || !passwordValid
 }
 
-// AQUI ADICIONA INTERAÇÃO AO BOTÃO ONDE ESCONDE OU MOSTRA A SENHA PARA O USUÁRIO
+// valida se o email inserido é válido
+function validateEmail(email) {
+  return /\S+@\S+.\S+/.test(email)
+}
 
+// adiciona interação ao botão onde mostra ou esconde a senha para o usuário
 let containers = document.querySelectorAll('.input-box')
 let inputs = document.querySelectorAll('.senha')
 let icons = document.querySelectorAll('.toggle-icon')
@@ -47,7 +86,7 @@ for (let i = 0; i < icons.length; i++) {
   })
 }
 
-// AQUI DEFINE QUE O BOTÃO "VOLTAR" VOLTE A PAGINA ANTERIOR EM QUE O USUARIO ESTAVA
+// define que o botão "voltar" volte a página anterior em que o usuário estava
 function goBack() {
   window.history.back()
 }
